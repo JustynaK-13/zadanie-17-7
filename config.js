e323cf8e23f53b7129b328eae6d8445609af1018
Justyna Kaczorowska <js.kaczorowska@gmail.com>
2018-05-24 13:25:55 +0200

    Change name of files and add '' to config

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-	'GOOGLE_CLIENT_ID':749669576651-gi3c7d88vujoh9qbatv6ck81kucr45mu.apps.googleusercontent.com
-	'GOOGLE_CLIENT_SECRET':eSa-TbLcZDtAOwBF00xD2j2s
+	'GOOGLE_CLIENT_ID':'749669576651-gi3c7d88vujoh9qbatv6ck81kucr45mu.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET':'eSa-TbLcZDtAOwBF00xD2j2s',
 	'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
 };
